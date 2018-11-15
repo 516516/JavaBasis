349e6344ef19a6aa4a0c70e3261404b965b4c987
516516 <2082344874@qq.com>
2018-11-15 16:45:10 +0800

    类与对象

--- src/demo6_2_4/TestTranspose.java
+++ src/demo6_2_4/TestTranspose.java
@@ -7,19 +7,44 @@ public class TestTranspose {
 		int result[][]=transpose(array);
 		for(int i=0;i<result.length;i++) {
 			for(int j=0;j<result[i].length;j++) {
-				System.out.print(result[i][j]);
+				System.out.print(result[i][j]+" ");
 			}
 			System.out.println();
 		}
+		
+		int sumResult[][]=getColumnSum(array);
+		for(int i=0;i<sumResult.length;i++) {
+			for(int j=0;j<sumResult[i].length;j++) {
+				System.out.print(sumResult[i][j]+" ");
+			}
+		}
+	}
+	
+	public TestTranspose() {
+		this(3);
+	}
+	
+	public TestTranspose(int a) {
 	}
 	
 	public static int[][] transpose(int array[][]){
 		int result[][]=new int[array[0].length][array.length];
-		
 		for(int i=0;i<array.length;i++) {
 			for(int j=0;j<array[i].length;j++) {
 				result[j][i]=array[i][j];
 			}
+		}
+		return result;
+	}
+	
+	public static int[][] getColumnSum(int array[][]){ 
+		int result[][]=new int[1][array[0].length];
+		for(int i=0;i<array[0].length;i++) {
+			int sum=0;
+			for(int j=0;j<array.length;j++) {
+				sum+=array[i][j];
+			}
+			result[0][i]=sum;
 		}
 		return result;
 	}
